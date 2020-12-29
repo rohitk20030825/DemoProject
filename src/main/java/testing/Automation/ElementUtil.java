4dbe41e4e7c761e84ec1d6efcb5f47f37e7cb1aa
rohitk20030825 <rohitk20030825@gmail.com>
2020-12-29 19:59:59 +0530

    Changes

---
+++ src/main/java/testing/Automation/ElementUtil.java
@@ -0,0 +1,15 @@
+package testing.Automation;
+
+import org.openqa.selenium.WebDriver;
+
+public class ElementUtil {
+
+	WebDriver driver;
+	String locatorType;
+	String locatorValue;
+	
+	public ElementUtil(WebDriver driver,String locatorType,String locatorValue) {
+		
+	}
+		
+}
--- src/test/java/testing/Automation/LoginPageTest.java
+++ src/test/java/testing/Automation/LoginPageTest.java
@@ -1,12 +1,53 @@
 package testing.Automation;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class LoginPageTest {
 
 	@Test
-	public void testing() {
-		System.out.println("Hello world!");
+	public void testing() throws MalformedURLException {
+		
+		RemoteWebDriver driver = null;
+		try{
+			ChromeOptions options = new ChromeOptions();
+			DesiredCapabilities capabilities = new DesiredCapabilities(options);
+			URL url = new URL("http://localhost:5555/wd/hub");
+			driver = new RemoteWebDriver(url,capabilities);
+			driver.get("http://www.gmail.com");
+			System.out.println("Title of page:"+driver.getTitle());
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		driver.quit();
+	}
+	
+	
+	
+	@Test
+	public void testing1() throws MalformedURLException {
+		
+		RemoteWebDriver driver = null;
+		try{
+			FirefoxOptions options = new FirefoxOptions();
+			DesiredCapabilities capabilities = new DesiredCapabilities(options);
+			URL url = new URL("http://localhost:4444/wd/hub");
+			driver = new RemoteWebDriver(url,capabilities);
+			driver.get("http://www.gmail.com");
+			System.out.println("Title of page:"+driver.getTitle());
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		driver.quit();
 	}
 	
 }
